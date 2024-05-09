b059ba0928f1e7324eb3febde572a03a462b4cda
skogsgren <6107181+skogsgren@users.noreply.github.com>
2024-05-09 17:23:47 +0200

    fix relative dir for makescript

--- make.sh
+++ make.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-VENV_PATH="crawler_venv"
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+VENV_PATH="${SCRIPT_DIR}/crawler_venv"
 TMP_PATH="/tmp"
 
 source "${VENV_PATH}/bin/activate"
 scp "nas:~/sssb_feed.xml" "${TMP_PATH}/sssb_feed.xml"
-python3 "sssb.py" "${TMP_PATH}/sssb_feed.xml"
+python3 "${SCRIPT_DIR}/sssb.py" "${TMP_PATH}/sssb_feed.xml"
 if [ $? != 0 ]; then
 	notify-send "Failed to crawl SSSB"
 else
-	scp "${TMP_PATH}sssb_feed.xml" "nas:~/sssb_feed.xml"
+	scp "${TMP_PATH}/sssb_feed.xml" "nas:~/sssb_feed.xml"
 	notify-send "Successfully updated SSSB RSS feed"
 fi
 deactivate
