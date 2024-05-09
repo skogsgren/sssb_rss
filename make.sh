d4dfcdd977ddff60f337032acc072ab508ac55a9
skogsgren <6107181+skogsgren@users.noreply.github.com>
2024-05-09 13:06:21 +0200

    update usage instructions

--- make.sh
+++ make.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-source crawler_venv/bin/activate
-scp nas:~/sssb_feed.xml /tmp/sssb_feed.xml
-python3 sssb.py /tmp/sssb_feed.xml
+VENV_PATH="crawler_venv"
+TMP_PATH="/tmp"
+
+source "${VENV_PATH}/bin/activate"
+scp "nas:~/sssb_feed.xml" "${TMP_PATH}/sssb_feed.xml"
+python3 "sssb.py" "${TMP_PATH}/sssb_feed.xml"
 if [ $? != 0 ]; then
 	notify-send "Failed to crawl SSSB"
 else
-	scp /tmp/sssb_feed.xml nas:~/sssb_feed.xml
+	scp "${TMP_PATH}sssb_feed.xml" "nas:~/sssb_feed.xml"
 	notify-send "Successfully updated SSSB RSS feed"
 fi
 deactivate
